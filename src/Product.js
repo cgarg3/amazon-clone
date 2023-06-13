3634cdda3f2476c09ebb99592d1ce73f34ac31b7
Your Name <you@example.com>
2023-06-13 13:10:54 -0400

    initial commit

---
+++ src/Product.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import "./Product.css"
+
+function Product({title, image, price, rating}) {
+  return (
+    <div className='product'>
+      <div className='product-info'>
+        <p>{title}</p>
+        <p className="product-price">
+            <small>$</small>
+            <strong>{price}</strong>
+        </p>
+        <div className="product-rating">
+          {Array(rating).fill().map((_,i) => (
+        <p>🌟</p>
+          ))}
+        </div>
+      </div>
+        <img src={image} alt="" className='product-image'/>
+        <button>Add to Basket</button>
+    </div>
+  )
+}
+export default Product
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCatq38KvXsreeBKrhJRfseSz7XieFsD-A",
+    authDomain: "clone-71813.firebaseapp.com",
+    projectId: "clone-71813",
+    storageBucket: "clone-71813.appspot.com",
+    messagingSenderId: "556995747311",
+    appId: "1:556995747311:web:7e2ce811d2c23df476ea1a",
+    measurementId: "G-SDR0NKT5QX"
+  };
